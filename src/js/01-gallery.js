1ae70538b230bda45738c6e388b979cccc93d3a4
Irina40Sigova <mishinaira1@gmail.com>
2023-01-28 22:40:34 +0200

    init push

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,36 @@
-// Add imports above this line
-import { galleryItems } from './gallery-items';
-// Change code below this line
+import { galleryItems } from './gallery-items.js';
+
+import "simplelightbox/dist/simple-lightbox.min.css";
+
+import SimpleLightbox from  "simplelightbox";
 
 console.log(galleryItems);
+
+const paletteGallery = document.querySelector('.gallery');
+const galleryMarkup = createGalleryItems (galleryItems);
+
+
+paletteGallery.insertAdjacentHTML('beforeend', galleryMarkup);
+
+function createGalleryItems (items) {
+    return items.map(({preview, original, description}) => {
+        return `
+      <a class="gallery__item" href="${original}">
+          <img
+            class="gallery__image"
+            src="${preview}"
+            alt="${description}"
+          />
+        </a>
+      `;
+    }).join('');
+    
+};
+
+
+
+new SimpleLightbox('.gallery a',{
+    captionsData: 'alt' ,
+    captionDelay: 250,
+   });
+
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,45 @@
 
+
+ import throttle  from  "lodash.throttle";
+  
+
+ const form = document.querySelector('.feedback-form');
+ const textarea = document.querySelector('.feedback-form textarea');
+ 
+ const formData = {};
+ 
+   form.addEventListener('submit',onFormSubmit);
+   form.addEventListener('input', throttle (onFormInput),500);
+ 
+  function onFormSubmit(e){
+      e.preventDefault();
+       
+      e.target.reset();
+       localStorage.removeItem('feedback-form-state');
+  };
+ 
+ 
+   function onFormInput ({target}){
+    const email = target.name;
+    const message = target.value;
+ //    formData[target.name]=  target.value;
+    formData[email] = message;
+   
+    const formDataJSON = JSON.stringify(formData)
+    console.log(formDataJSON);
+ 
+     localStorage.setItem('feedback-form-state', formDataJSON);
+ };
+ 
+ 
+ messageOutput();
+ 
+ function messageOutput (){
+   const messageSave = localStorage.getItem('feedback-form-state');
+ 
+   if (messageSave){
+     textarea.value =messageSave;
+   }
+ };
+ 
+ 
